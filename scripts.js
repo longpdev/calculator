ba03da91dc0e10fe8b889aae9529259b76933622
longpdev2 <longpdev@gmail.com>
2022-11-06 15:58:09 -0600

    handle clicking non-numbers element

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
+const result = document.querySelector('.result-container');
+const number = document.querySelector('.numbers');
+var displayValue = 0;
+var currentValue = "";
+
+function display(){
+    
+}
+number.addEventListener('click', (e) =>{
+    if (e.target.value != null)
+    {    
+        displayValue = e.target.value;
+        currentValue += displayValue;
+        result.innerText += displayValue;
+    }
+} );
+
 function add(a, b) {
     return a + b;
 }
